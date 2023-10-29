5d75e6b350eee66429f704c8425ce0b5e6fe69af
Gopinath <gopinaath.work@gmail.com>
2023-10-29 09:40:20 -0700

    added useTransition -20:33

--- components/AddProductButton.tsx
+++ components/AddProductButton.tsx
@@ -1,15 +1,22 @@
 "use client";
 
+import { addProductToDatabase } from "@/actions/serverActions";
 import { useTransition } from "react";
 
 function AddProductButton(){
 
     const [isPending, startTransition] = useTransition();
 
+    const formData = new FormData();
+    formData.append("product", "Macbook Pro");
+    formData.append("price", "1299.99");
+
     return(
         <button
-            
-        >Add Product
+          onClick={() => startTransition(() => addProductToDatabase(formData))}
+          className="fixed bottom-10 right-10 border bg-green-500 text-white p2 rounded-md w-48"
+        >
+            {isPending? "Adding...":"Add Macbook Pro"}
         </button>
     )
 }
